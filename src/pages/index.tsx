c3cc7e7bce7ee22d9d4f7c920169c3904fdbb591
Leeseunghwan7305 <sun1238799@naver.com>
2022-11-26 17:09:39 +0900

    feat: useQuery로 상품목록 가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { getAllProduct } from '@/api/api';
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
 const Product = () => {
-  useEffect(() => {
-    console.log(getAllProduct());
-  }, []);
-  return <div>메인페이지</div>;
+  const { data } = useQuery(['shop'], getAllProduct);
+  console.log(data);
+  return <div></div>;
 };
 
 export default Product;
